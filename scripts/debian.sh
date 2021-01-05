aeba00bc349c15d2c43ba944acca99bcfc3813fb
Keller Davis <keller.davis@biblegateway.com>
2021-01-05 12:54:29 -0800

    remove oh-my-zsh. git is funny when shared

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -51,62 +51,11 @@ apt-get install -y \
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
-# code shim, it fallbacks to code-insiders if code is not available
-cat << 'EOF' > /usr/local/bin/code
-#!/bin/sh
-get_in_path_except_current() {
-  which -a "$1" | grep -v "$0" | head -1
-}
-code="$(get_in_path_except_current code)"
-if [ -n "$code" ]; then
-  exec "$code" "$@"
-elif [ "$(command -v code-insiders)" ]; then
-  exec code-insiders "$@"
-else
-  echo "code or code-insiders is not installed" >&2
-  exit 127
-fi
+cat << 'EOF' >> /root/.zshrc
+autoload -U colors
+colors
+alias ls='ls --color=auto'
+PROMPT="%F{6}%d%f %F{8}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%f"$'\n'"%F{5}❯%f "
+export HISTFILE=~/.zsh_history
+SAVEHIST=10000
 EOF
-chmod +x /usr/local/bin/code
-
-# oh-my-zsh
-CODESPACES_ZSH="$(cat \
-<<EOF
-prompt() {
-    if [ ! -z "\${GITHUB_USER}" ]; then
-        local USERNAME="@\${GITHUB_USER}"
-    else
-        local USERNAME="%n"
-    fi
-    PROMPT="%{\$fg[green]%}\${USERNAME} %(?:%{\$reset_color%}➜ :%{\$fg_bold[red]%}➜ )"
-    PROMPT+='%{\$fg_bold[blue]%}%~%{\$reset_color%} \$(git_prompt_info)%{\$fg[white]%}$ %{\$reset_color%}'
-}
-ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[cyan]%}(%{\$fg_bold[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{\$fg_bold[yellow]%}✗%{\$fg_bold[cyan]%})"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{\$fg_bold[cyan]%})"
-prompt
-EOF
-)"
-
-# Adapted Oh My Zsh! install step to work with both "Oh Mys" rather than relying on an installer script
-# See https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh for offical script.
-
-umask g-w,o-w
-mkdir -p /root/.oh-my-zsh
-git clone --depth=1 \
-    -c core.eol=lf \
-    -c core.autocrlf=false \
-    -c fsck.zeroPaddedFilemode=ignore \
-    -c fetch.fsck.zeroPaddedFilemode=ignore \
-    -c receive.fsck.zeroPaddedFilemode=ignore \
-    https://github.com/ohmyzsh/ohmyzsh /root/.oh-my-zsh 2>&1
-echo -e "$(cat "/root/.oh-my-zsh/templates/zshrc.zsh-template")\nDISABLE_AUTO_UPDATE=true\nDISABLE_UPDATE_PROMPT=true" > /root/.zshrc
-
-sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="codespaces"/g' /root/.zshrc
-mkdir -p /root/.oh-my-zsh/custom/themes
-echo "${CODESPACES_ZSH}" > /root/.oh-my-zsh/custom/themes/codespaces.zsh-theme
-
-# Shrink git while still enabling updates
-cd /root/.oh-my-zsh
-git repack -a -d -f --depth=1 --window=1
