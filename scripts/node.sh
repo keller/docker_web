d1de1f7ad4e9ae101a1aa873ff4839fad10da310
Keller Davis <keller.davis@biblegateway.com>
2023-07-27 09:21:46 -0700

    re-add node for cypress and newman tests

---
+++ scripts/node.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+NODE_DIR=${1}
+NODE_VERSION=${2}
+
+#mkdir node-build && curl --silent -L https://github.com/nodenv/node-build/archive/refs/tags/v4.9.33.tar.gz | tar xz -C node-build --strip-components 1
+git clone https://github.com/nodenv/node-build.git /root/node-build
+PREFIX=/usr/local /root/node-build/install.sh
+rm -rf /root/node-build
+node-build $NODE_VERSION $NODE_DIR/v$NODE_VERSION
+
+npm install --global yarn@1.22.10
+npm install --global cypress@12.17.2
+npm install --global mocha@10.2.0
+npm install --global newman@5.3.2
